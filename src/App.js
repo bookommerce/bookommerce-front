06fb3cb1cc46db5ee6d571992911dae2b579e9a7
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-01-26 22:49:01 -0300

    feat: home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BookDetails from "./Components/BookDetails/BookDetails";
+import Home from './Components/Home/Home';
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path="/book/:idBook" element={<BookDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
