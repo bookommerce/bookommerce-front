fda5d6b3a675ed0b642ef7348eb30b315a0349f1
Marcele Montalvão <mmontalvaoblog@outlook.com>
2023-01-28 15:29:15 -0300

    fix: UserContext and Signin Page

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,20 +1,3 @@
-import { createContext, useState } from "react";
+import { createContext } from "react";
 
 export const UserContext = createContext({});
-
-export const UserProvider = ({ children }) => {
-    const [disabled, setDisabled] = useState(false)
-    const [loading, setLoading] = useState(false)    
-    const [user, setUser] = useState(undefined)
-    const [token, setToken] = useState(undefined)
-
-    if(loading) {
-        return null;
-    }
-
-    return (
-        <UserContext.Provider value={{ user, setUser, loading, setLoading, disabled, setDisabled, token, setToken }}>
-            {children}
-        </UserContext.Provider>
-    )
-}
