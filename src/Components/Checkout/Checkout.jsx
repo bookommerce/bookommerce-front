7357f2ffc48764294aefaa7df86c3b3c954c6eca
Marcele Montalvão <mmontalvaoblog@outlook.com>
2023-01-30 03:15:21 -0300

    feat: render products cart in checkout page

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,19 +1,41 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ThreeDots } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+import { api } from "../../services/api";
 import Header from "../Header/Header";
 import { CheckoutStyle, HomeButton, MainDiv, PageTitle, PaymentStatus, Thank } from "./CheckoutStyles";
 import SalesData from "./SalesData";
 
-
 export default function Checkout() {
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
-
+    const { token } = useContext(UserContext);
+    const [books, setBooks] = useState(undefined)
     setTimeout(() => setLoading(false), 2000);
 
+    useEffect(()=> { 
+        const getCartProducts = async () => {
+            try {
+                const response = await api.get(`/cart`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    } 
+                });
+                
+                if(!response) {
+                    return null;
+                } else if (response && response.status === 200) {
+                    setBooks(response.data)
+                }
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getCartProducts()
+    }, [token])
 
-
+    console.log(books)
     return (
         <>
             <Header />
@@ -46,8 +68,9 @@ export default function Checkout() {
                             <SalesData user={{ Name: "Fulano" }}
                                 address={{ State: "RJ", City: "rio de janeiro", Address: "XV de novembro 234", ZipCode: "65300-234" }}
                                 payment={{ Name: "Fulano", Number: "1234-1234-1234-1245", Validity: "01/24", CVC: "123" }}
-                                books={[{ Image: "https://m.media-amazon.com/images/I/41897yAI4LL._SY344_BO1,204,203,200_QL70_ML2_.jpg", Name: "Harry Potter e a Pedra Filosofal", Price: "23,12" },
-                                {Image:"https://m.media-amazon.com/images/I/41897yAI4LL._SY344_BO1,204,203,200_QL70_ML2_.jpg", Name:"Harry Potter e a Pedra Filosofal", Price:"23,12"}]} />
+                                books={
+                                   books.map((book) => book.book)
+                                } />
                             <HomeButton onClick={() => navigate('/home')}>Voltar a Home</HomeButton>
                         </>
 
